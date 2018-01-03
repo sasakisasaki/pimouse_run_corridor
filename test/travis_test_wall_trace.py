d2e33a5ac54fd4afca8b29a341ff2ad969759786
Junya Sasaki <j.straight.hopeful@gmail.com>
2018-01-03 17:26:08 +0900

    Add files (pimouse_run_corridor)

--- test/travis_test_wall_trace.py
+++ test/travis_test_wall_trace.py
@@ -18,21 +18,23 @@ class WallStopTest(unittest.TestCase):
         return left, right
 
     def test_io(self):
-        left, right = self.set_and_get(400,100,100,0)
+        left, right = self.set_and_get(400,0,0,100)
         self.assertTrue(left == right == 0, "cannot be stopped")
 
-        left, right = self.set_and_get(40,0,0,9)
-        self.assertTrue(0 < left == right < 1000, "cannot be moving again")
+        left, right = self.set_and_get(0,5,1000,0)
+        self.assertTrue(left == right != 0, "stopped wrongly by side-sensors")
 
-        time.sleep(5.0)
-        left, right = self.set_and_get(40,0,0,9)
-        self.assertTrue(2000 < left == right, "cannot be accerelated")
+        left, right = self.set_and_get(0,10,0,0)
+        self.assertTrue(left < right, "do not curve to left")
 
-        left, right = self.set_and_get(15,0,20,15)
-        self.assertTrue(left == right == 0, "cannot be stopped again")
+        left, right = self.set_and_get(0,200,0,0)
+        self.assertTrue(left > right, "do not curve to right")
+
+        left, right = self.set_and_get(0,5,0,0)
+        self.assertTrue(0 < left == right, "curved wrongly")
 
 
 if __name__ == '__main__':
     time.sleep(3)
-    rospy.init_node('travis_test_wall_stop_accel')
-    rostest.rosrun('pimouse_run_corridor','travis_test_wall_stop_accel',WallStopTest)
+    rospy.init_node('travis_test_wall_trace')
+    rostest.rosrun('pimouse_run_corridor','travis_test_wall_trace',WallStopTest)
